1a1054176c8adf5f073ed65cf2e50e3e2f89fca3
sengggggg <64160299@go.buu.ac.th>
2022-12-21 17:34:11 +0700

    Update php_01.php

---
+++ public_html/php_01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+        $n = 12;
+        
+        ?>
+
+        
+    </body>
+</html>
