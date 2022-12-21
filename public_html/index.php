c1fa79250884279ed458069b8136dfed0354ad21
sengggggg <64160299@go.buu.ac.th>
2022-12-21 19:45:03 +0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,25 @@
 
 <body>
     <?php
-        echo "Hello";
+    // Scope GLOBAL
+        $x = 4;
+        function assignx()
+        {
+            // Scope LOCAL           
+            $x = 0;
+            print "\$x inside function is $x. <br />";
+            global $x; // Decare to use GLOBAL        
+            print "\$x (2) inside function is $x. <br />";
+            // STATIC        
+            static $a = 0;
+            $a++;
+            echo "\$a is $a\n";
+            echo "<br>";
+        }
+        assignx();
+        assignx();
+        assignx();
+        print "\$x outside of function is $x. <br />";
     ?>
 </body>
 
