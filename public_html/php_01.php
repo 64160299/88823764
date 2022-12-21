4eb9fb2ce22b93ffd24d16e02c8eb92d3cb35661
sengggggg <64160299@go.buu.ac.th>
2022-12-21 19:45:06 +0700

    Update php_01.php

--- public_html/php_01.php
+++ public_html/php_01.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"/>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <?php
         $n = 12;
+        echo '<table border="1px solid">
+        <th>';
         
+        for ($i = 0 ; $i <= $n ; $i++) {
+            echo '<td>', $i, '</td>';
+        }
+        echo '</th>';
+        echo '<tr>';
+        echo '<td>', $n , '</td>';
+        for ($i = 0; $i <= $n; $i++) {
+            echo '<td>', $i * $n, '</td>';
+        }
+        echo '</tr>';
+        echo '</table>';
         ?>
 
-        
+
     </body>
 </html>
