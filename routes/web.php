7e71f80f0de64bd660a30d5708f3c9b9e1178196
sengggggg <64160299@go.buu.ac.th>
2023-01-04 19:16:43 +0700

    l

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-route/{input_data?}', function ($input_data = "") {
+    return view('my_view');
+});
